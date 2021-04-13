9e245c0468c34b9f43eec1411457eddef2d2d557
Noah <nheindev@gmail.com>
2021-04-12 21:50:23 -0500

    stuck here for a bit, i'm Tired

--- blockchain/block.go
+++ blockchain/block.go
@@ -4,58 +4,71 @@ import (
 	"bytes"
 	"encoding/gob"
 	"log"
-)
+	crypto/sha256"
 
-// Block is a single unit in the blockchain
-type Block struct {
+
+// Block is a singl unit in the blockchain
+type Block struc {
 	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-	Nonce    int
-}
+	Transactions []Transaction
+	PrevHash []bte
+	once    int
 
-// Commenting out this code as our new proof of work algorithm takes care of the hashing functionality of the blocks
 
-// DeriveHash will hash the data of the current block, along with the hash from the block preceding it
+/ Commenting out this code as our new proof of work algorithm takes care of the hashing functionality of the blocks
+
+// DeriveHash will hash the dataof the current block, along with the hash from the block preceding it
 //func (b *Block) DeriveHash() {
-//info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-//hash := sha256.Sum256(info)
-//b.Hash = hash[:]
+//info := bytes.Join([][]byteb.Data, b.PrevHash}, []byte{})
+//hash := sha256.Sm256(info)
+//bHash = hash[:]
 //}
-///////////////////////////////////////
+//////////////////////////////////////
 
-// CreateBlock creates a block and performs a proof of work algorithm
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash, 0}
-	pow := NewProofOfWork(block)
-	nonce, hash := pow.Run()
+// CreateBlock creates a block and performs a proof of ork algorithm
+func CreateBlock(txs []*Transaction, prevHash []byte)*lock {
+	block := &Block{[]byte{}, tx, prevHash, 0}
+	pow := NewProofOfWork(blck)
+nonce, hash := pow.Run()
 
-	block.Hash = hash[:]
-	block.Nonce = nonce
+	block.Hash = hash[:
+block.Nonce = nonce
 
-	return block
+	eturn block
+
+
+// Genesis needs to be he first block in a chain, as the first block doesn't have an address to point back to
+func Genesis(coinbase *Transaction) *Blok {
+	eturn CreateBlock([]*Transaction{coinbse}, []byte{})
+
+
+func (b *Block) HashTansactions() []byte {
+	var txHashes [][]byte 
+var txHash [32]byte 
+
+for _, tx := range b.Transactions {
+		txHashes =append(txHashes, tx.ID)
+}
+	txHash = sha256.Su256(bytes.Join(txHashes, []byte))
+
+return txHash[:]
 }
 
-// Genesis needs to be the first block in a chain, as the first block doesn't have an address to point back to
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func (b *Block) Serialize() []byte {
-	var res bytes.Buffer
-	encoder := gob.NewEncoder(&res)
+func (b *Block) erialize() []byte {
+	vr res bytes.Buffer
+	ncoder := gob.NewEncoder(&res)
 
 	err := encoder.Encode(b)
 
-	Handle(err)
+Handle(err)
 
-	return res.Bytes()
+return res.Bytes()
 }
 
-func Handle(err error) {
-	if err != nil {
-		log.Panic(err)
-	}
+func Handle(rr error) {
+if err != nil {
+		log.Panic(er)
+	
 }
 
 func Deserialize(data []byte) *Block {
--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"encoding/gob"
 	"fmt"
+	"log"
 )
 
 //reward is the amnount of tokens given to someone that "mines" a new block
@@ -16,18 +17,31 @@ type Transaction struct {
 	Outputs []TxOutput
 }
 
-//Tx Output represents the output of a transaction in the blockchain
+//TxOutput represents a transaction in the blockchain
+//For Example, I sent you 5 coins. Value would == 5, and it would have my unique PubKey
 type TxOutput struct {
 	Value int
 	//Value would be representative of the amount of coins in a transaction
 	PubKey string
+	//The Pubkey is needed to "unlock" any coins within an Output. This indicated that YOU are the one that sent it.
+	//You are indentifiable by your PubKey
 	//PubKey in this iteration will be very straightforward, however in an actual application this is a more complex algorithm
 }
 
+//Important to note that each output is Indivisible.
+//You cannot "make change" with any output.
+//If the Value is 10, in order to give someone 5, we need to make two five coin outputs.
+
+//TxInput is represntative of a reference to a previous TxOutput
 type TxInput struct {
-	ID  []byte
+	ID []byte
+	//ID will find the Transaction that a specific output is inside of
 	Out int
+	//Out will be the index of the specific output we found within a transaction.
+	//For example if a transaction has 4 outputs, we can use this "out" field to specify which output we are looking for
 	Sig string
+	//This would be a script that adds data to an outputs' PubKey
+	//however for this tutorial the Sig will be indentical to the PubKey.
 }
 
 func (tx *Transaction) SetID() {
@@ -36,7 +50,9 @@ func (tx *Transaction) SetID() {
 
 	encoder := gob.NewEncoder(&encoded)
 	err := encoder.Encode(tx)
-	Handle(err)
+	if err != nil {
+		log.Panic(err)
+	}
 
 	hash = sha256.Sum256(encoded.Bytes())
 	tx.ID = hash[:]
@@ -52,10 +68,21 @@ func CoinbaseTx(toAddress, data string) *Transaction {
 	//This means that we initialize it with no ID, and it's OutputIndex is -1
 	txIn := TxInput{[]byte{}, -1, data}
 	//txOut will represent the amount of tokens(reward) given to the person(toAddress) that executed CoinbaseTx
-	txOut := TxOutput{reward, toAddress}
+	txOut := TxOutput{reward, toAddress} // You can see it follows {value, PubKey}
 
 	tx := Transaction{nil, []TxInput{txIn}, []TxOutput{txOut}}
 
 	return &tx
 
+}
+func (tx *Transaction) IsCoinbase() bool {
+	//This checks a transaction and will only return true if it is a newly minted "coin"
+	// Aka a Coinbase transaction
+	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
+}
+func (in *TxInput) CanUnlock(data string) bool {
+	return in.Sig == data
+}
+func (out *TxOutput) CanBeUnlocked(data string) bool {
+	return out.PubKey == data
 }
